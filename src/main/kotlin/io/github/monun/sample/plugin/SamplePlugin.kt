1ebff94c84584b8783e0110489471281253b9c41
Silver <antdaniel101@gmail.com>
2023-05-15 15:58:13 +0200

    Change

--- src/main/kotlin/io/github/monun/sample/plugin/SamplePlugin.kt
+++ src/main/kotlin/io/github/monun/sample/plugin/SamplePlugin.kt
@@ -7,10 +7,5 @@ import org.bukkit.plugin.java.JavaPlugin
 class SamplePlugin : JavaPlugin() {
     override fun onEnable() {
         logger.info("Hello world!")
-
-        println(ServerPlayer::class.java)
-        for (declaredField in Entity::class.java.declaredFields) {
-            println(declaredField.name)
-        }
     }
 }
